4f4478918fa79ba6fb6ab87e17aea8101e575c9f
Ridhuvarshini M <ridhuvarshini@sen-sei.in>
2021-12-09 16:52:59 +0530

    2nd commit

---
+++ src/test/java/GitHubPushAndImport/GitHubExperiment.java
@@ -0,0 +1,14 @@
+package GitHubPushAndImport;
+import org.testng.annotations.Test;
+
+public class GitHubExperiment {
+	
+		@Test
+		
+		public void demoTest()
+		
+		{
+			System.out.println("My first git Hub Script 1");
+		}
+	}
+
